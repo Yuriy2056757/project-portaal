1e190a011cc80c75ad68f545acc69cfb99caa800
Yuriy2056757 <2056757@talnet.nl>
2019-09-12 15:09:37 +0000

    Fix error

--- app/Project.php
+++ app/Project.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $fillable = [
-        'title', 'description',
-    ]
+        'title', 'description'
+    ];
 }
