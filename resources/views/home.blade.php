5fac77460937bbe1dcba6b24c4074387d509b047
Yuriy2056757 <2056757@talnet.nl>
2019-09-12 15:06:51 +0000

    Add Project resource

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,10 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <a class="btn btn-primary" href="{{ route('projects.create') }}">NIEUW PROJECT</a>
+
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Projecten</div>
 
                 <div class="card-body">
                     @if (session('status'))
