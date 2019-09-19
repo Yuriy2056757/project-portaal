7be1311658295ddf607dd971e68d875f68e45c2d
MrDvs <1998dennis@live.nl>
2019-09-19 13:20:37 +0200

    Created an User seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
+            'student_id' => rand(40000, 80000),
+            'email' => Str::random(10).'@talnet.nl',
+            'first_name' => 'Willem',
+            'last_name' => 'de Jong',
+            'class_name' => 'OITAOO7B',
+            'password' => bcrypt('password'),
         ]);
     }
 }
