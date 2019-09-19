25dee13a3d24681aa9c8b1c717edb43c0dc29e63
MrDvs <1998dennis@live.nl>
2019-09-19 13:57:36 +0200

    Fixed a small error in the merges

--- app/User.php
+++ app/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
 
     public function projects()
     {
-        $this->belongsTo('App\Project', ' project_user', 'user_id', 'project_id');
+        $this->belongsTo('App\Project', 'project_user', 'user_id', 'project_id');
     }
 	public function comments()
     {
